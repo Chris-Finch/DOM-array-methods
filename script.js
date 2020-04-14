5f226a2850b25cf99aeee543569f15b93d3b9fc8
Chris Finch <tophfinch@gmail.com>
2020-04-14 11:53:39 -0600

    reduce/add wealth

--- script.js
+++ script.js
@@ -49,6 +49,22 @@ function sortByRichest() {
     updateDOM();
 }
 
+// filter users by millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+// Calculate total wealth displayed
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Update DOM
 function updateDOM(providedData = data) {
     // Clear main div
@@ -71,3 +87,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
